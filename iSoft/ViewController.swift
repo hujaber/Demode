b19a7e542646e365d19b19c5e25cbbea782e189b
hujaber <hujaber@me.com>
2016-09-24 09:38:10 +0300

    Initial Commit

---
+++ iSoft/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iSoft
+//
+//  Created by Hussein Jaber on 24/9/16.
+//  Copyright © 2016 Hussein Jaber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
